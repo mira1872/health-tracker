b027d7dbf8a6f7e1acdf255a52f91d024e713ba4
MartinaShreif <157921700+MartinaShreif@users.noreply.github.com>
2024-06-25 03:41:56 +0300

    Get & Update user data integrations
    3azama

--- Front-end/js/logout.js
+++ Front-end/js/logout.js
@@ -1,5 +1,6 @@
 document.getElementById('log-out').addEventListener('click', function (e) {
     e.preventDefault();
     localStorage.removeItem('jwt');
+    localStorage.removeItem('id');
     window.location.href = 'index.html';
 })
